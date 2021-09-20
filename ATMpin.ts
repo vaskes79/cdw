893fd4270fce6e31337c1a3d426a59989a3b4b8c
Vasily Guzov <vasily.guzov@gmail.com>
2021-09-20 15:46:56 +0300

    validate pin code best solution community

--- ATMpin.ts
+++ ATMpin.ts
@@ -1,9 +1,13 @@
 class Kata {
   static validatePin(pin: string): boolean {
-    const regPattern = /^[\d]+$/;
-    const lengthPin = [...pin].length;
-    return (lengthPin === 4 || lengthPin === 6) && regPattern.test(pin);
+    const pinPattern: RegExp = new RegExp(/^\d{4}(\d{2})?$/);
+    return pinPattern.test(pin);
   }
 }
 
-console.log(Kata.validatePin("1234"));
+console.log(Kata.validatePin("1234"), "1234 true");
+console.log(Kata.validatePin("123456"), "123456 true");
+console.log(Kata.validatePin("1234567"), "1234567 false");
+console.log(Kata.validatePin("a1234"), "a1234 false");
+console.log(Kata.validatePin("234"), "234 false");
+console.log(Kata.validatePin("1"), "1 false");
